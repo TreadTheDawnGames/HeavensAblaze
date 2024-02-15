ea56b91833d33df19c650725e651d7e9169447e1
GANONDORF\CASPI <caspian@tylerinternet.com>
2024-02-14 23:45:17 -0500

    Ship Tracking works on server

--- Assets/Scripts/targeting system/Target.cs
+++ Assets/Scripts/targeting system/Target.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Target : MonoBehaviour
 {
     public PredictionMotor targetShip;
     public new RectTransform transform;
+
+    public TMP_Text distanceDisplay;
 
     public Target(PredictionMotor targetShip)
     {
--- Assets/Scripts/targeting system/TargetingHud.cs
+++ Assets/Scripts/targeting system/TargetingHud.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using FishNet.Object;
 using FishNet.Managing;
+using UnityEngine.UI;
 
 public class TargetingHud : NetworkBehaviour
 {
@@ -20,14 +21,16 @@ public class TargetingHud : NetworkBehaviour
     List<GameObject> targets = new List<GameObject>();
     List<PredictionMotor> ships = new List<PredictionMotor>();
 
+    PredictionMotor ship;
+
     private void Start()
     {
-        
+        ship = transform.root.GetComponent<PredictionMotor>();
     }
 
     private void Update()
     {
-        foreach(PredictionMotor ship in FindObjectsOfType<PredictionMotor>())
+        foreach (PredictionMotor ship in FindObjectsOfType<PredictionMotor>())
         {
             if (ship.transform != transform.root)
             {
@@ -36,27 +39,49 @@ public class TargetingHud : NetworkBehaviour
                     ships.Add(ship);
                     GameObject target = Instantiate(targetPrefab, canvas.transform, false);
                     target.GetComponent<Target>().targetShip = ship;
+
                     targets.Add(target);
                 }
             }
         }
 
-        foreach (GameObject target in targets)
+        foreach (GameObject targetPre in targets)
         {
-            if (target.GetComponent<Renderer>().isVisible)
+            Target target = targetPre.GetComponent<Target>();
+
+            if (target.GetComponent<Target>().targetShip != null)
             {
-                gameObject.SetActive(true);
+                
+                Renderer renderer = target.GetComponent<Target>().targetShip.GetComponentInChildren<Renderer>();
+                if (renderer == null)
+                {
+                    continue;
+                }
+
+                bool vis = renderer.isVisible;
+                if(target.targetShip == null || !target.targetShip.gameObject.activeInHierarchy)
+                {
+                    //find whether the ship is dead so it works on client.
+                    //currently the PredictionMotor does not deactivate, the main body and its children do
+                    //meaning this chunk of code doesn't register the ship as dead
+                    vis = false;
+                }
+
+                target.GetComponent<Image>().enabled = vis;
+                target.distanceDisplay.gameObject.SetActive(vis);
+                
+
+                target.distanceDisplay.text = Vector3.Distance(ship.transform.position, target.targetShip.transform.position).ToString("0.00") + "m";
+
+                if (!gameObject.activeInHierarchy)
+                    gameObject.SetActive(true);
 
                 Vector3 shipPosition = shipCam.WorldToScreenPoint(target.GetComponent<Target>().targetShip.gameObject.transform.position);
-                print("Ship position: " + shipPosition);
-                print("target transform position: " + target.transform.position);
+                
 
-                target.transform.position = shipPosition;
-            }
-            else
-            {
-                gameObject.SetActive(false);
+                RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.GetComponent<RectTransform>(), shipPosition, shipCam, out Vector2 localPoint);
 
+                target.GetComponent<RectTransform>().localPosition = localPoint;
             }
 
         }
