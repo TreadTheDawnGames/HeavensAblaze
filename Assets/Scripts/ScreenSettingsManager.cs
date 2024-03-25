6480aef09ab211a7a9fadb87063afc1a1f4ee764
GANONDORF\CASPI <caspian@tylerinternet.com>
2024-03-25 11:28:49 -0400

    Full screen button/dropdown resolutions added

---
+++ Assets/Scripts/ScreenSettingsManager.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+using System.Linq;
+
+public class ScreenSettingsManager : MonoBehaviour
+{
+
+    public bool fullScreen = false;
+    public GameObject fullScreenTickmark;
+    [SerializeField]
+    Button fullScreenTickbox;
+    [SerializeField]
+    TMP_Dropdown resolutionsDropdown;
+    List<Resolution> resolutions = new List<Resolution>();
+
+    private void Start()
+    {
+        SetupResolutions();
+        fullScreenTickbox.onClick.AddListener(() => ToggleFullScreen());
+        resolutionsDropdown.onValueChanged.AddListener((Int) => SetResolution(Int));
+        fullScreen = Screen.fullScreen;
+    }
+    private void OnDestroy()
+    {
+        fullScreenTickbox.onClick.RemoveAllListeners();
+        resolutionsDropdown.onValueChanged.RemoveAllListeners();
+
+    }
+    public void ToggleFullScreen()
+    {
+        fullScreen = !fullScreen;
+        fullScreenTickmark.SetActive(fullScreen);
+        Screen.fullScreen = fullScreen;
+        
+    }
+
+    private void SetupResolutions()
+    {
+        List<string> resolutionStrings = new List<string>();
+
+        foreach (Resolution resolution in Screen.resolutions)
+        {
+            resolutions.Add(resolution);
+        }
+        resolutions.Reverse();
+
+        foreach(Resolution resolution in resolutions)
+        {
+            resolutionStrings.Add(resolution.ToString());
+        }
+        //resolutionStrings.Reverse();
+        resolutionsDropdown.AddOptions(resolutionStrings);
+    }
+
+    void SetResolution(Int32 index)
+    {
+        
+        Screen.SetResolution(resolutions[index].width, resolutions[index].height, fullScreen);
+    }
+
+}
